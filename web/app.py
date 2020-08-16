3b9ba0960ed7bf8b2dc6875457c3dd8ccb94ab63
icyc0re <tiziano.signo@gmail.com>
2020-08-16 21:20:22 +0200

    add new POST resource to mine a block

--- web/app.py
+++ web/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
-from core.blockchain import Blockchain
+from flask import Flask, jsonify
+from core.blockchain import (
+  Blockchain,
+  compute_proof_of_work,
+  compute_block_hash)
 
 app = Flask(__name__)
 
@@ -9,3 +12,15 @@ blockchain = Blockchain()
 def start_server():
   print('Starting server...')
   app.run()
+
+@app.route('/mine_block', methods=['POST'])
+def mine_block():
+  last_block = blockchain.get_last_block()
+  proof = compute_proof_of_work(last_block.proof)
+  previous_hash = compute_block_hash(last_block)
+  block = blockchain.create_block(proof, previous_hash)
+  response = {
+    'message': 'Congratulations, you just mined a block!',
+    'block': block.__dict__
+  }
+  return jsonify(response), 201
