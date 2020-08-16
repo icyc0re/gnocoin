8dc39b36637a1a18079970c1e3f9035c9f3e99d7
icyc0re <tiziano.signo@gmail.com>
2020-08-16 20:53:16 +0200

    add computation of proof of work (outside of chain), add function to validate the blockchain as not corrupted

--- core/blockchain.py
+++ core/blockchain.py
@@ -1,14 +1,32 @@
 from hashlib import sha256
 from datetime import datetime
+import json
 
 MAX_PROOF = 2 ** 16
 TARGET_HASH = 2 ** 240 # digest with 4 leading zeros
 
-def compute_hash(body):
+def compute_hash(body: str):
+  """ Compute hash of any string body """
   return sha256(str(body).encode()).hexdigest()
 
-def is_valid_hash(digest):
-  return int(digest, 16) < TARGET_HASH
+def compute_block_hash(block):
+  return sha256(json.dumps(block, sort_keys=True, separators=(',', ':')).encode()).hexdigest()
+
+def is_valid_hash(hexdigest):
+  return int(hexdigest, 16) < TARGET_HASH
+
+def proof_computation(proof, previous_proof):
+  return proof**2 - previous_proof**2
+
+def compute_proof_of_work(previous_proof):
+  """ Calculate the proof of work (mine) """
+  new_proof = 1
+  while new_proof < MAX_PROOF:
+    new_hash = compute_hash(proof_computation(new_proof, previous_proof))
+    if is_valid_hash(new_hash):
+      return new_proof
+    new_proof += 1
+  return None
 
 class Blockchain:
   def __init__(self):
@@ -16,6 +34,7 @@ class Blockchain:
     self.create_block(proof = 1, previous_hash = '0')
   
   def create_block(self, proof, previous_hash):
+    """ Create a new block and append it at the end of the chain """
     block = {
       'index': len(self.chain) + 1,
       'timestamp': str(datetime.now()),
@@ -27,14 +46,23 @@ class Blockchain:
     return block
 
   def get_previous_block(self):
+    """ Get the last block """
     return self.chain[-1]
   
-  def proof_of_work(self, previous_proof):
-    new_proof = 1
-    while new_proof < MAX_PROOF:
-      new_hash = compute_hash(new_proof**2 - previous_proof**2)
-      if is_valid_hash(new_hash):
-        return new_proof
-      new_proof += 1
-    return None
+  def is_chain_valid(self):
+    """
+    Check if the blockchain is valid
     
+    Checks:
+    - block index is sequentially increasing
+    - the "previous_hash" entry matches the hash of the previous node
+    - the proof of work results in a valid hash
+    """
+    previous_block = self.chain[0]
+    for block in self.chain[1:]:
+      if (block['index'] != previous_block['index'] + 1 or
+          block['previous_hash'] != compute_block_hash(previous_block) or
+          not is_valid_hash(compute_hash(proof_computation(block['proof'], previous_block['proof'])))):
+        return False
+      previous_block = block
+    return True
